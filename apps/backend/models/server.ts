6e9e287153db0d07e470753b28bc4bff32d5d442
Cesar Arellano <raywayday@gmail.com>
2024-05-23 15:36:40 -0600

    create my server with express

---
+++ apps/backend/models/server.ts
@@ -0,0 +1,19 @@
+import express, { Application } from "express";
+
+class Server {
+  private app: Application;
+  private port: string;
+
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || "8000";
+  }
+
+  listen(): void {
+    this.app.listen(this.port, () => {
+      console.log("Server is running on port: ", this.port);
+    });
+  }
+}
+
+export default Server;
