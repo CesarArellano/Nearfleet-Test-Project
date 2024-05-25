afd28b1f1dbffb56ea1a2193430f9bea6b1f4d72
Cesar Arellano <raywayday@gmail.com>
2024-05-24 20:13:53 -0600

    fix environment file for web app

--- apps/frontend/lib/config/constants/environment.dart
+++ apps/frontend/lib/config/constants/environment.dart
@@ -1,9 +1,10 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
-  static String baseURL = (Platform.isAndroid)
-    ? dotenv.env['BASE_URL_FOR_ANDROID_ONLY'] ?? 'http://:10.0.2.2:8000/api'
-    : dotenv.env['BASE_URL'] ?? 'http://localhost:8000/api';
+  static String baseURL = (kIsWeb || !Platform.isAndroid)
+    ? dotenv.env['BASE_URL'] ?? 'http://localhost:8000/api'
+    : dotenv.env['BASE_URL_FOR_ANDROID_ONLY'] ?? 'http://:10.0.2.2:8000/api';
 }
